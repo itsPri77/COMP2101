a4568c88b66bef0cab3fd1afe93175a6eb64dedd
Priyanka Subedi <priyasubedi77@gmail.com>
2023-02-15 17:20:36 -0500

    Lab 2 Completed

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-echo "FQDN: $(hostname)"
-hostnamectl status
-ipv4=$(hostname -I)
-ipv6=$(ip addr | grep -w inet6 | awk '{print$2}' | grep -v ::1/128 )
-echo "IP Addresses: $ipv4 $ipv6"
-echo "Root Filesystem Status: "
-df -h /
+fqdn=$(hostname -f)  #This displays the FQDN of the machine
+os=$(lsb_release -ds)   #This will print the description of the distribution.
+ip=$(ip route show | awk 'NR==1 {print$3}')    #This shows all the ip routes and awk filters it.
+fs=$(df -h / | awk 'NR==2 {print $4}')         #This list the amount of disk space used and awk filters it to get just required value
+cat << EOF
+
+Report for $(hostname | cut -d '.' -f1 )
+======================
+FQDN: $fqdn
+Operating System name and version: $os
+IP Address: $ip
+Root Filesystem Free Space: $fs
+======================
+
+EOF
